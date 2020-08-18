0cc12f09f0602c6a5a96f47ab485d86aa1056f98
AlbertUlysses <albertulysseschavez@gmail.com>
2020-08-17 21:40:31 -0700

    test: update test

--- excerises/chapter1.py
+++ excerises/chapter1.py
@@ -1,15 +1,19 @@
 import re
+from random import randrange
+
+
 class Game:
 
 
-    def __init__(self, lower_bound, upper_bound, random_value):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.random_value = random_value
+    def __init__(self, lower_bound, upper_bound):
+        self.random_value = randrange(lower_bound, upper_bound)
+    
+    def random_game():
+        pass
+
 
     def valid_input(self, input_value):
         pattern = '[0-9]+'
         if re.search(pattern, input_value):
             return True
-        else:
-            return False
+        raise Exception('Input needs to be a number')
--- tests/test_excerise1.py
+++ tests/test_excerise1.py
@@ -1,18 +1,15 @@
 from excerises.chapter1 import Game
 import pytest
 
-game1 = Game(0, 100, 5)
-   
-guess_number = 5
-   
-assert guess_number > game1.lower_bound
-assert guess_number < game1.upper_bound
 
 def test_valid_input():
 
-    game1 = Game(0, 100, 5)
+    game1 = Game(0, 100)
    
-    guess_number = 5
+    guess_number = '5'
 
-    assert game1.valid_input('k') == False
-    assert game1.valid_input('5') == True
+    assert game1.valid_input(guess_number)
+    with pytest.raises(Exception) as exception_info:
+        game1.valid_input('k')
+    assert exception_info.match('Input needs to be a number')
+
